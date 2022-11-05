fe3f35eb27effe7654fca27cfdaea3927c565114
Zach <zacharyyazzie94@gmail.com>
2022-11-04 20:26:38 -0600

    working user routes

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const NotesController = require('../controllers/notes');
 const usersController = require('../controllers/users');
 const validation = require('../middleware/validate');
 const loadUser = require('../middleware/loadUser');
 
 router.use([loadUser]);
 
-router.get('/all', usersController.getAllUsers);
+router.get('/', usersController.getUserInfo);
 
-router.get('/:postId', usersController.getOneUser);
-
-router.post('/', validation.user, usersController.createUser);
-
-router.put('/:postId', validation.user, usersController.updateUser);
-
-router.delete('/:postId', usersController.deleteUser);
+router.delete('/delete/:postId', usersController.deleteUser);
 
 module.exports = router;
